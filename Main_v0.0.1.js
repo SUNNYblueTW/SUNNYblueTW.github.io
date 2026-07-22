d3fcae9914532c541f804ac0cdad1254d91d900a
SUNNYblueTW <sunnybluetw0@gmail.com>
2026-07-23 05:04:10 +0800

    Main_v0.0.1.js

---
+++ Main_v0.0.1.js
@@ -0,0 +1,4 @@
+const to_my_Youtube_channel=document.getElementById("to_my_Youtube_channel_button");
+to_my_Youtube_channel.onclick=function(){
+  window.open("https://www.youtube.com/@SUNNYblueTW");
+};
